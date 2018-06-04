ec22e10c2bfa01092b32fb98d0adfa7a726dfe1e
Ryan Parnas <ryanbgstl@gmail.com>
2018-06-03 19:15:37 -0500

    Working search algorithm

--- sum-search.js
+++ sum-search.js
@@ -3,7 +3,7 @@ let console = document.getElementById('console');
 let insert_example_button = document.getElementById('insert-example-button');
 let insert_example_count_box = document.getElementById('insert-example-count-box');
 let numbers_box = document.getElementById('numbers-box');
-let percent_tolerance_box = document.getElementById('percent-tolerance-box');
+let tolerance_box = document.getElementById('tolerance-box');
 let start_button = document.getElementById('start-button');
 let status_box = document.getElementById('status-box');
 let stop_button = document.getElementById('stop-button');
@@ -16,11 +16,12 @@ var search = null;
 /* Setup */
 let setEnabledness = running => {
   insert_example_button.disabled = running;
+  insert_example_count_box.disabled = running;
   start_button.disabled = running;
   stop_button.disabled = !running;
   numbers_box.disabled = running;
-  percent_tolerance_box.disabled = running;
   target_sum_box.disabled = running;
+  tolerance_box.disabled = running;
 };
 
 /* Console */
@@ -31,17 +32,22 @@ let clearConsole = () => {
 };
 
 let writeError = message => {
-  let div = document.createElement('div');
-  div.classList.add('text-danger');
-  div.textContent = message;
-  console.appendChild(div);
+  let e = document.createElement('div');
+  e.classList.add('text-danger');
+  e.textContent = message;
+  console.appendChild(e);
 };
 
 let writeMessage = message => {
-  let div = document.createElement('div');
-  div.textContent = message;
-  console.appendChild(div);
+  let e = document.createElement('div');
+  e.textContent = message;
+  console.appendChild(e);
 };
+
+let writeEmpty = message => {
+  let e = document.createElement('br');
+  console.appendChild(e);
+}
 
 /* Search */
 let spawnThread = (work, onKill) => {
@@ -60,8 +66,8 @@ let spawnThread = (work, onKill) => {
   };
   
   let makeStatusString = () => {
-    var ret = killed ? "Done" : "Running";
-    ret += " (" + Math.floor(pctComplete*100) + "% in " + makeTimeString((Date.now() - start)) + ")";
+    var ret = killed ? "Stopped" : "Running";
+    ret += " (" + Math.floor(pctComplete*100) + "% of possibilities checked in " + makeTimeString((Date.now() - start)) + ")";
     ret += killed ? "." : "...";
     return ret;
   };
@@ -101,33 +107,43 @@ let makeSearchWorker = (numbers, sumMin, sumMax) => {
   var i = 0;
   var bitsValue = 0;
   
-  let increment = () => {
-    while (i < bits.length) {
-      if (bits[i])
-      {
-        sum -= numbers[i];
-        bits[i] = false;
-        i = i + 1;
-      }
-      else
-      {
-        sum += numbers[i];
-        bits[i] = true;
-        i = 0;
-        break;
-      }
-      
-      bitsValue++;
-    }
-  };
-  
-  var iteration = 0;
   var sum = 0;
   var max =  Math.pow(2, bits.length)  - 1;
   
   return () => {   
-    for (var x = 0; x < 200000; x++) {  
-      increment();
+    for (var x = 0; x < 1000000 && bitsValue < max; x++) {
+      // Increment by one
+      while (i < bits.length) {
+        if (bits[i])
+        {
+          sum -= numbers[i];
+          bits[i] = false;
+          i = i + 1;
+        }
+        else
+        {
+          sum += numbers[i];
+          bits[i] = true;
+          i = 0;
+          break;
+        }
+      }
+      bitsValue++;
+      
+      // Check for sums
+      if (sum >= sumMin && sum <= sumMax)
+      {
+        var subset = [];
+        for (var j = 0; j < bits.length; j++)
+        {
+          if (bits[j])
+          {
+            subset.push(numbers[j]);
+          }
+        }
+
+        writeMessage("* { " + subset.join(', ') + " } sums to " + sum);
+      }
     }
     return bitsValue / max;
   };
@@ -141,59 +157,77 @@ let makeTimeString = ms => {
 insert_example_button.onclick = () => {
   let numberCount = parseFloat(insert_example_count_box.value);
   
+  status_box.textContent = "";
   clearConsole();
   if (isNaN(numberCount) || numberCount == 0) {
     writeError('The number of examples to insert must be an integer greater than 0.');
   }
   
+  let rng = new MersenneTwister(randomSeed++);
+  let rand = () => rng.genrand_real1();
   let numberMin = 0;
   let numberMax = 10000;
   let numberRange = numberMax - numberMin;
-  let targetSum = numberRange * numberCount;
-  let percentTolerence = 5;
-  let rng = new MersenneTwister(randomSeed++);
-  let randomNumber = () => Math.floor(numberMin + (rng.genrand_real1() * numberRange));
+  let targetSum = Math.floor(numberRange/3) * numberCount + .33;
+  let tolerance = 0.05;
+  let randomNumber = () => Math.floor(numberMin + (rand() * numberRange)) + Math.floor(rand() * 100) / 100;
   
   let numbers = Array.from({ length: numberCount }, randomNumber).sort((a, b) => a - b);
   
   numbers_box.value = numbers.join(', ');
-  percent_tolerence_button = '' + percentTolerence;
+  tolerance_box.value = tolerance;
   target_sum_box.value = targetSum;
 };
 
 start_button.onclick = () => {
-  let percentTolerence = parseFloat(percent_tolerance_box.value);
+  let tolerance = parseFloat(tolerance_box.value);
   let targetSum = parseFloat(target_sum_box.value);
-  let numbers = numbers_box.value.split(',').map(n => parseFloat(n));
+  let numbers = numbers_box.value.split(',').map(n => parseFloat(n)).sort((a, b) => b - a);
   
   clearConsole();
   
   // Input error-checking.
   var errors = 0;
+  
   if (numbers.some(isNaN)) {
     errors++;
     writeError('Numbers must be a comma-separated list of numbers.'); 
   }
+  
   if (isNaN(targetSum)) {
     errors++;
     writeError('Target Sum must be a number.');
   }
-  if (!Number.isInteger(percentTolerence) || percentTolerence < 0 || percentTolerence > 100) {
+  
+  if (isNaN(tolerance)) {
     errors++;
-    writeError('Percent Tolerance must be a value between 0% and 100%.');
+    writeError('tolerance must be a number.');
   }
+  
+  let sumMin = targetSum - tolerance;
+  let sumMax = targetSum + tolerance;
+  let sumOfAllNumbers = numbers.reduce((total, n) => total + n);
+  
+  if (sumOfAllNumbers < sumMin) {
+    errors++;
+    writeError(`The sum of all numbers is ${sumOfAllNumbers} which is too small to reach sums in the range ${sumMin} to ${sumMax}`);
+  };
+  
   if (errors > 0) {
     return;
   }
+
+  var what = tolerance == 0 ? `with a sum of ${targetSum}` : `with a sum in the range ${sumMin} to ${sumMax}`;
+  var nums = '{ ' + numbers.join(', ') + ' }';
+  writeEmpty();
+  writeMessage(`Subsets of ${nums} ${what}:`);
+  writeEmpty();
   
-  let sumMin = targetSum - ((percentTolerence * targetSum) / 100);
-  let sumMax = targetSum + ((percentTolerence * targetSum) / 100);
+  setEnabledness(true);
   let worker = makeSearchWorker(numbers, sumMin, sumMax);
   stopSearch = spawnThread(worker, () => {
     setEnabledness(false);
   });
-  
-  setEnabledness(true)
 };
 
 stop_button.onclick = () => {
